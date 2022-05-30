373725d08185bf171b8447172baf2290d2182828
PelucheRojo <106445669+PelucheRojo@users.noreply.github.com>
2022-05-29 21:04:36 -0600

    Create Par.java

---
+++ Par.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Par
+{
+    public static void main(String[] args){ 
+        Scanner entrada= new Scanner(System.in);
+        int n;
+        System.out.println("Ingrese el numero a evaluar: ");
+        n=entrada.nextInt();
+        
+        if(n%2==0){
+            System.out.println("Es un numero par.");
+        }
+        else{
+            System.out.println("El numero es impar.");
+        }
+        
+        }
+    }
+
+<!---
+PelucheRojo/PelucheRojo is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
